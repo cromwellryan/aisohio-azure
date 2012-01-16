9e7af0f0e6f6a928fab348c232e1cbeb2de45458
Ryan Cromwell <ryan@echelontouch.com>
2012-01-15 22:39:22 -0500

    updated

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('The AIS team says hello Dayton!\n');
+    res.end('Sup Dayton!\n');
 }).listen(port);
